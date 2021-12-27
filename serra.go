a60dcaabc342d333f422b0f11e1d91d21d601a44
Florian Baumann <flo@noqqe.de>
2021-12-27 11:09:18 +0100

    rename

---
+++ serra.go
@@ -0,0 +1,35 @@
+// Package main provides a typing test
+package main
+
+import (
+	"github.com/docopt/docopt-go"
+	"github.com/noqqe/serra/src/serra"
+)
+
+// Main Loop
+func main() {
+
+	usage := `Archivar
+
+Usage:
+  serra new <path>...
+	serra update <path>...
+  serra value <path>...
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+`
+
+	args, _ := docopt.ParseDoc(usage)
+
+	if args["new"].(bool) {
+		serra.New(args["<path>"].(string))
+	}
+	if args["update"].(bool) {
+		for _, i := range args["<path>"].([]string) {
+			serra.Update(i)
+		}
+	}
+
+}
---
+++ src/serra/set.go
@@ -0,0 +1,6 @@
+package serra
+
+type Set struct {
+	Description string   `description`
+	Cards       []string `cards`
+}
