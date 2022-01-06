b0f597e8f61b2a80e8d446ab837d0633f4fed2e5
Florian Baumann <flo@noqqe.de>
2022-01-06 14:02:20 +0100

    support multi add

--- add.fish
+++ add.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/env fish
 
 function serra_add ()
-    ./serra add $argv[1]/$argv[2]
-    echo ./serra add $argv[1]/$argv[2] >> add.log
+    set -l args (string split " " $argv)
+    if test -n "$args[3]"
+      ./serra add $args[1]/$args[2] --count=$args[3]
+      echo ./serra add $args[1]/$args[2] --count=$args[3] >> add.log
+    else
+      ./serra add $args[1]/$args[2]
+      echo ./serra add $args[1]/$args[2] >> add.log
+    end
 end
 
 while true;
