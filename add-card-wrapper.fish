0099fed6e619b5035cf1fdaa8dd04c96d44d9a6a
Florian Baumann <flo@noqqe.de>
2022-11-28 16:42:32 +0100

    fix add-card-wrapper.fish

--- add-card-wrapper.fish
+++ add-card-wrapper.fish
@@ -1,13 +1,23 @@
 #!/usr/bin/env fish
 
 function serra_add ()
+
+    # split input args
     set -l args (string split " " $argv)
+
+    # check if user wants to exit
+    if [ "$args[2]" = "exit" ]
+      exit 0
+    end
+
+    # if usage contains a number at the end, add multiple ones
+    # if not, add a single one
     if test -n "$args[3]"
-      ./serra add $args[1]/$args[2] --count=$args[3]
-      echo ./serra add $args[1]/$args[2] --count=$args[3] >> add.log
+      serra add $args[1]/$args[2] --count=$args[3] -u
+      echo serra add $args[1]/$args[2] --count=$args[3] >> add.log
     else
-      ./serra add $args[1]/$args[2]
-      echo ./serra add $args[1]/$args[2] >> add.log
+      serra add $args[1]/$args[2] -u
+      echo serra add $args[1]/$args[2] >> add.log
     end
 end
 
