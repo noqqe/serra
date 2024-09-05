0c575021b33284687ab0003866a3ee2a0adc269e
Florian Baumann <flo@noqqe.de>
2024-09-05 11:34:55 +0200

    fix check output

--- pkg/serra/check.go
+++ pkg/serra/check.go
@@ -59,16 +59,16 @@ func checkCards(cards []string, detail bool) error {
 		// If Card is in collection, print yes.
 		if len(co) >= 1 {
 			c := co[0]
-			fmt.Printf("PRESENT %s \"%s\" (%s, %.2f%s)", card, c.Name, c.Rarity, c.getValue(foil), getCurrency())
+			fmt.Printf("PRESENT %s \"%s\" (%s, %.2f%s)\n", card, c.Name, c.Rarity, c.getValue(foil), getCurrency())
 			continue
 		} else {
 			if detail {
 				// fetch card from scyrfall if --detail was given
 				c, _ := fetchCard(setName, collectorNumber)
-				fmt.Printf("MISSING %s \"%s\" (%s, %.2f%s)", card, c.Name, c.Rarity, c.getValue(foil), getCurrency())
+				fmt.Printf("MISSING %s \"%s\" (%s, %.2f%s)\n", card, c.Name, c.Rarity, c.getValue(foil), getCurrency())
 			} else {
 				// Just print, the card name was not found
-				fmt.Printf("MISSING \"%s\"", card)
+				fmt.Printf("MISSING \"%s\"\n", card)
 			}
 		}
 	}
