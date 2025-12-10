4bab2d5541d746c6d4f96f15b7fd700b6773842e
Florian Baumann <flo@noqqe.de>
2025-12-10 08:09:16 +0100

    Switch from Gingonic to Chi

--- pkg/serra/web.go
+++ pkg/serra/web.go
@@ -5,7 +5,9 @@ import (
 	"strconv"
 	"text/template"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
 	"github.com/spf13/cobra"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,6 +19,7 @@ func init() {
 	rootCmd.AddCommand(webCmd)
 }
 
+// needed for template functions, as Go templates don't support basic math operations
 func add(a, b int64) int64 {
 	return a + b
 }
@@ -28,7 +31,10 @@ var webCmd = &cobra.Command{
 	Long:          "Start a tiny web interface to have a web view of your collection",
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		startWeb()
+		err := startWeb()
+		if err != nil {
+			return err
+		}
 		return nil
 	},
 }
@@ -41,81 +47,132 @@ type Query struct {
 	Page  int64  `form:"page"`
 }
 
-func startWeb() error {
-	router := gin.Default()
-	router.SetFuncMap(template.FuncMap{
-		"add": add,
-	})
-	router.LoadHTMLGlob("templates/*.tmpl")
-	router.Static("/assets", "./assets")
-
-	// Landing page
-	router.GET("/", landingPage)
-
-	router.Run(address + ":" + strconv.FormatUint(port, 10))
-	return nil
+type TemplateData struct {
+	Title    string
+	Cards    []Card
+	Sets     []SetsResult
+	Query    Query
+	Version  string
+	PrevPage int64
+	Page     int64
+	NextPage int64
+	Limit    int64
+	NumCards int64
+	NumPages int64
 }
 
-func landingPage(c *gin.Context) {
+// StartWeb initializes the web server with Chi
+func startWeb() error {
+	router := chi.NewRouter()
+
+	// Use middlewares
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	// Define static assets directory
+	fs := http.FileServer(http.Dir("./assets"))
+	router.Handle("/assets/*", http.StripPrefix("/assets", fs))
+
+	// Landing page route
+	router.Get("/", indexPage)
+
+	// Start the server
+	return http.ListenAndServe(address+":"+strconv.FormatUint(port, 10), router)
+}
+
+// indexPage handles the landing page, rendering the template with the appropriate data
+func indexPage(w http.ResponseWriter, r *http.Request) {
+
+	// Setting up template functions
+	templates := template.New("templates")
+	templates.Funcs(template.FuncMap{
+		"add": add,
+	})
+
+	// Load the template files
+	tmpl, err := templates.ParseGlob("templates/*.gohtml")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// Parse query parameters
 	var query Query
-	if c.ShouldBind(&query) == nil {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Failed to parse query parameters", http.StatusBadRequest)
+		return
+	}
+	query.Set = r.FormValue("set")
+	query.Sort = r.FormValue("sort")
+	query.Name = r.FormValue("name")
+	pageStr := r.FormValue("page")
+	if pageStr != "" {
+		query.Page, _ = strconv.ParseInt(pageStr, 10, 64)
+	}
 
-		// Construct per Page results "limit"
-		strLimit := c.DefaultQuery("limit", "500")
-		limit, _ := strconv.ParseInt(strLimit, 10, 64)
-		if limit == 0 {
-			limit = 500
-		}
+	// Construct per Page results "limit"
+	strLimit := r.URL.Query().Get("limit")
+	limit, _ := strconv.ParseInt(strLimit, 10, 64)
+	if limit == 0 {
+		limit = 500
+	}
 
-		// Fetch all sets for Dropdown
-		sets := Sets("release")
+	// Fetch all sets for Dropdown
+	sets := Sets("release")
 
-		// Fetch all results based on filter criteria
-		cards := Cards("", query.Set, query.Sort, query.Name, "", "", false, false, query.Page*int64(limit), limit, "", "")
+	// Fetch all results based on filter criteria
+	cards := Cards("", query.Set, query.Sort, query.Name, "", "", false, false, query.Page*int64(limit), limit, "", "")
 
-		// Construct quick way for counting results
-		filter := bson.D{}
-		client := storageConnect()
-		coll := client.getCardsCollection()
+	// Construct quick way for counting results
+	filter := bson.D{}
+	client := storageConnect()
+	coll := client.getCardsCollection()
 
-		if query.Set != "" {
-			filter = append(filter, bson.E{"set", query.Set})
-		}
+	if query.Set != "" {
+		filter = append(filter, bson.E{"set", query.Set})
+	}
 
-		if query.Name != "" {
-			filter = append(filter, bson.E{"name", bson.D{{"$regex", ".*" + query.Name + ".*"}, {"$options", "i"}}})
-		}
+	if query.Name != "" {
+		filter = append(filter, bson.E{"name", bson.D{{"$regex", ".*" + query.Name + ".*"}, {"$options", "i"}}})
+	}
 
-		counts, _ := coll.AggregateCards(mongo.Pipeline{
-			bson.D{
-				{"$match", filter},
-			},
-			bson.D{
-				{"$group", bson.D{
-					{"_id", nil},
-					{"count", bson.D{{"$sum", 1}}},
-				}}},
-		})
-		defer storageDisconnect(client)
+	counts, err := coll.AggregateCards(mongo.Pipeline{
+		bson.D{
+			{"$match", filter},
+		},
+		bson.D{
+			{"$group", bson.D{
+				{"_id", nil},
+				{"count", bson.D{{"$sum", 1}}},
+			}}},
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
-		// Catch index error on no results
-		var numCards int32
-		if len(counts) != 0 {
-			numCards = counts[0]["count"].(int32)
-		}
+	defer storageDisconnect(client)
 
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":    "Serra",
-			"cards":    cards,
-			"sets":     sets,
-			"query":    query,
-			"version":  Version,
-			"prevPage": query.Page - 1,
-			"page":     query.Page,
-			"nextPage": query.Page + 1,
-			"limit":    limit,
-			"numCards": int64(numCards),
-			"numPages": int64(numCards) / limit,
-		})
+	// Catch index error on no results
+	var numCards int32
+	if len(counts) != 0 {
+		numCards = counts[0]["count"].(int32)
+	}
+
+	tmplData := TemplateData{
+		Title:    "Serra",
+		Cards:    cards,
+		Sets:     sets,
+		Query:    query,
+		Version:  Version,
+		PrevPage: query.Page - 1,
+		Page:     query.Page,
+		NextPage: query.Page + 1,
+		Limit:    limit,
+		NumCards: int64(numCards),
+		NumPages: int64(numCards) / limit,
+	}
+
+	err = tmpl.ExecuteTemplate(w, "index.gohtml", tmplData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
